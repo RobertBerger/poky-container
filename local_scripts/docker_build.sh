40e8201827923974bc68570b2db1bd9d55a2dae8
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2019-11-19 14:02:04 +0200

    attempt to add my standard scripts

---
+++ local_scripts/docker_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source ../container-name.sh
+
+if [ $# -lt 1 ];
+then
+    echo "+ $0: Too few arguments!"
+    echo "+ use something like:"
+    echo "+ $0 <docker image>" 
+    echo "+ $0 reslocal/${CONTAINER_NAME}"
+    exit
+fi
+
+pushd ..
+export BASE_DISTRO="ubuntu-16.04"
+export REPO="reslocal/crops-poky-container"
+./build-and-test.sh
+popd
