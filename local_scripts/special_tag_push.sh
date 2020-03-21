9be141424b6dda9d295e86d35962f9a7d3416a1d
Robert Berger <robert.berger@ReliableEmbeddedSystems.com>
2020-03-21 13:52:06 +0200

    gui support

--- local_scripts/special_tag_push.sh
+++ local_scripts/special_tag_push.sh
@@ -1,7 +1,7 @@
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Usage:"
     echo "./special_tag_push.sh <tag>"
-    echo "./special_tag_push.sh ubuntu-16.04"
+    echo "./special_tag_push.sh ubuntu-16.04 ubuntu-16.04-gui"
     exit
 fi
 
@@ -9,8 +9,8 @@ source ../container-name.sh
 
 set -x
 docker images
-docker tag reslocal/${CONTAINER_NAME}:$1 reliableembeddedsystems/${CONTAINER_NAME}:$1
+docker tag reslocal/${CONTAINER_NAME}:$1 reliableembeddedsystems/${CONTAINER_NAME}:$2
 docker images
 docker login --username reliableembeddedsystems
-docker push reliableembeddedsystems/${CONTAINER_NAME}:$1
+docker push reliableembeddedsystems/${CONTAINER_NAME}:$2
 set +x
